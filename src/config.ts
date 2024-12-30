c722a2d9cd0418f54d923b515c76396d7fb1da2f
oborozuk <github-action@actions-template-sync.noreply.github.com>
2024-12-30 08:07:30 +0000

    chore(template): merge template changes :up:
    Signed-off-by: oborozuk <github-action@actions-template-sync.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -7,11 +7,11 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Oborozuki',
-  subtitle: '一个普通的博客',
-  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  title: 'Fuwari',
+  subtitle: 'Demo Site',
+  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 15,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
@@ -44,31 +44,33 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/oborozuk',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Oborozuki',
-  bio: '一个普通大学生的博客',
+  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Lorem Ipsum',
+  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
-      name: 'Mail',
-      icon: 'material-symbols:mail-rounded',
-      url: 'mailto:3046652718@qq.com',
+      name: 'Twitter',
+      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+                                        // You will need to install the corresponding icon set if it's not already included
+                                        // `pnpm add @iconify-json/<icon-set-name>`
+      url: 'https://twitter.com',
     },
     {
-      name: 'bilibili',
-      icon: 'fa6-brands:bilibili',
-      url: 'https://space.bilibili.com/204487541',
+      name: 'Steam',
+      icon: 'fa6-brands:steam',
+      url: 'https://store.steampowered.com',
     },
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/oborozuk',
+      url: 'https://github.com/saicaca/fuwari',
     },
   ],
 }
