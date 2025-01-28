467b98868c73c5585b5fcc68bbc565b47f36ff31
oborozuk <github-action@actions-template-sync.noreply.github.com>
2025-01-28 06:05:09 +0000

    chore(template): merge template changes :up:
    Signed-off-by: oborozuk <github-action@actions-template-sync.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.2124096.xyz",
+  site: "https://fuwari.vercel.app/",
   base: "/",
   trailingSlash: "always",
   integrations: [
